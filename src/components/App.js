2276551b076d75788b4f12e60bc22e81dd66fadb
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-01-15 10:04:16 -0800

    add router configuration

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react'
+import { Router, Route, Link, browserHistory } from 'react-router'
 import Home from './Home'
+import About from './About'
+import MatchDetail from './MatchDetail'
 import matches from '../fakeMatchData'
 import '../styles/App.css'
 
-//TODO -- this is a nice proof of concept.
-//TODO -- need to hookup with multiple matches
-//TODO -- sync to firebase
-//TODO -- add authentication
-//TODO
 class App extends Component {
   constructor(props) {
     super(props)
-    this.claimTicket = this.claimTicket.bind(this)
-    this.ticketAvailable = this.ticketAvailable.bind(this)
-
     this.state = {
       matches,
       user: { uid: "randomUID" }
     }
+
+    this.claimTicket = this.claimTicket.bind(this)
+    this.ticketAvailable = this.ticketAvailable.bind(this)
   }
 
   claimTicket(e, matchId) {
@@ -37,11 +35,24 @@ class App extends Component {
 
   render() {
     return (
-      <Home
-        matches={this.state.matches}
-        user={this.state.user}
-        claimTicket={this.claimTicket}
-      />
+      <Router history={browserHistory}>
+        <Route
+          path="/"
+          component={Home}
+          matches={this.state.matches}
+          user={this.state.user}
+           />
+        <Route
+          path="about"
+          component={About} />
+        <Route
+          path="/matches/:matchId"
+          component={MatchDetail}
+          matches={this.state.matches}
+          user={this.state.user}
+          claimTicket={this.claimTicket}
+        />
+      </Router>
     )
   }
 }
