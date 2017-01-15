14a9bd3c7594795a05e51927b767b7cd98b1cc4e
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-01-15 10:13:21 -0800

    add basic firebase support

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: 'AIzaSyAGt32afx0uk5uDIJlFxfKnRH0tqgV-jS',
+  authDomain: 'season-ticket-tracker.firebaseapp.com',
+  databaseURL: 'https://season-ticket-tracker.firebaseio.com',
+});
+
+export default base;
