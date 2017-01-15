e078b9cd187621b7357ac7907400138c341687b4
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-01-15 08:41:27 -0800

    fake match data

---
+++ src/fakeMatchData.js
@@ -0,0 +1,62 @@
+import logo from './imgs/logo.svg'
+const matches = {
+  'gameOne':{
+    location:'CenturyLink Field',
+    time:'1:00 PM',
+    date:'July 01 2017',
+    awayTeam:{
+      name: 'LA Galaxy',
+      img: logo
+    },
+    homeTeam:{
+      name: 'Seattle Sounders',
+      img: logo
+    },
+    matchType:'MLS',
+    available: true,
+    qtyTicketsAvailable: 1,
+    ticketPrice:'2000',
+    id:'diuf9ewfjw908e9f8wef',
+    claimedUserId: null,
+  },
+  'gameTwo': {
+    location:'CenturyLink Field',
+    time:'1:00 PM',
+    date:'July 20 2017',
+    awayTeam:{
+      name: 'Portland Timbers',
+      img: logo
+    },
+    homeTeam:{
+      name: 'Seattle Sounders',
+      img: logo
+    },
+    matchType:'MLS',
+    available: true,
+    qtyTicketsAvailable: 1,
+    ticketPrice:'2000',
+    id:'diuf9ewfjw908e9f8wef',
+    claimedUserId: null,
+  },
+  'gameThree': {
+    location:'CenturyLink Field',
+    time:'1:00 PM',
+    date:'July 05 2017',
+    awayTeam:{
+      name: 'Real Salt Lake',
+      img: logo
+    },
+    homeTeam:{
+      name: 'Seattle Sounders',
+      img: logo
+    },
+    matchType:'MLS',
+    available: true,
+    qtyTicketsAvailable: 1,
+    ticketPrice:'2000',
+    id:'diuf9ewfjw908e9f8wef',
+    claimedUserId: null,
+  }
+};
+
+export default matches
