a9568e1f08b7d1891b8ca25793424ff1868ba2c7
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-01-15 12:51:43 -0800

    update with detail page

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router'
 import App from './components/App';
+import MatchDetail from './components/MatchDetail'
 import './styles/index.css';
 
 const Root = () => {
   return (
     <Router history={browserHistory}>
       <Route path="/" component={App} />
+      <Route path="/matches/:matchId" component={MatchDetail} />
     </Router>
   )
 }
+
 ReactDOM.render(
   <Root />,
   document.getElementById('root')
