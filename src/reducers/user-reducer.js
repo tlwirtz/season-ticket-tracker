e7aa3cbec4e5ad174ed2415b6c3ceb631e160583
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-01-18 20:57:49 -0800

    login/out user buttons

--- src/reducers/user-reducer.js
+++ src/reducers/user-reducer.js
@@ -2,7 +2,13 @@ import * as types from '../actions/user-actions'
 
 export default (state = {}, action) => {
   switch(action.type) {
+    case types.USER_LOGIN_REQUEST_SUCCESS:
+      return Object.assign(state, action.payload)
+    case types.USER_LOGOUT_REQUEST_SUCCESS:
+      return Object.assign(state, {user: null, credential: null})
+    case types.USER_LOGIN_REQUEST:
+    case types.USER_LOGOUT_REQUEST:
     default:
-    return state;
+      return state;
   }
 }
