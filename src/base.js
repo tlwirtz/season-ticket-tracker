3375a46adf8205845ca530c4bfdce5c6aefb9276
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-01-18 20:57:02 -0800

    corrected API Key

--- src/base.js
+++ src/base.js
@@ -1,7 +1,7 @@
 import Rebase from 're-base';
 
 const base = Rebase.createClass({
-  apiKey: 'AIzaSyAGt32afx0uk5uDIJlFxfKnRH0tqgV-jS',
+  apiKey: 'AIzaSyAGt32afx0uk5uDIJlFxfKnRH0tqgV-jSg',
   authDomain: 'season-ticket-tracker.firebaseapp.com',
   databaseURL: 'https://season-ticket-tracker.firebaseio.com',
 });
