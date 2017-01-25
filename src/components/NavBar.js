45297defd4ccf8075293d0ab8814bc5b070285f1
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-01-24 20:08:52 -0800

    styling with class

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 import { connect } from 'react-redux'
+import '../styles/NavBar.css'
+import '../styles/Colors.css'
 
 class NavBar extends Component {
   render() {
     return (
-      <div>
-        <h1>Season Ticket Tracker</h1>
-        <h3>Seattle Sounders 2017 Season</h3>
+      <div className="nav-bar-container">
+        <div className="nav-bar-item nav-bar-heading">
+          <h1 className="nav-bar-title">Match Finder</h1>
+          <h3 className="nav-bar-subheading soft-grey-text">Seattle Sounders 2017 Season</h3>
+        </div>
 
         { this.props.user.user ?
-          <h3>[[Display Name Here]]</h3> :
-          <Link to="/login" > Login </ Link>
+          <h3 className="nav-bar-item" >[[Display Name Here]]</h3> :
+          <Link to="/login"  className="nav-bar-item" > Login </ Link>
         }
       </div>
     )
