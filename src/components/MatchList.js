44c8b0c4b59ff41879dd47f76e366e9173b7bd42
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-01-25 18:08:53 -0800

    add small animation, [WIP]

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -6,7 +6,7 @@ class MatchList extends Component {
     const items = Object.keys(this.props.matches).map((key) => {
       const bg = this.props.matches[key].awayTeam.img
       return (
-        <div key={key} className='match' style={{backgroundImage: `url(${bg})`}} >
+        <div key={key} className='match animated fadeInUp' style={{backgroundImage: `url(${bg})`}} >
           <Match
             matchData={this.props.matches[key]}
             {...this.props}/>
