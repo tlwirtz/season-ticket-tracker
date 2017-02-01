50ba188369d51840b21b115d6fe6e449c7d98fea
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-01-31 18:59:06 -0800

    alerts set-up

---
+++ src/reducers/alert-reducer.js
@@ -0,0 +1,21 @@
+import * as types from '../actions/alert-actions'
+
+const updateState = (state, payload) => {
+  const newState = {...state}
+  Object.keys(payload).forEach(key => {
+    newState[key] = payload[key]
+  })
+  return { ...state, ...newState }
+}
+
+export default (state = {}, action) => {
+  switch(action.type) {
+    case types.SHOW_ALERT:
+    case types.HIDE_ALERT:
+      return updateState(state, action.payload);
+      break;
+    default:
+      return state;
+      break;
+  }
+}
