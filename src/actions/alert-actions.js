3aa5fc00f63b7aa39261d222538a98363cce54da
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-01-31 19:01:04 -0800

    fix typo in function name

--- src/actions/alert-actions.js
+++ src/actions/alert-actions.js
@@ -1,12 +1,14 @@
 export const SHOW_ALERT = 'SHOW_ALERT';
 export const HIDE_ALERT = 'HIDE_ALERT';
 
-export const udpateAlert = config => {
+export const updateAlert = config => {
   const defaults = {
     type: HIDE_ALERT,
-    status: 'normal',
-    visible: 'false',
-    msg: 'No message provided.'
+    payload: {
+      status: 'normal',
+      visible: false,
+      msg: 'No message provided.'
+    }
   }
 
   return { ...defaults, ...config }
