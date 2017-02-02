68efcf93f60ad455f94b89271166d6c9ef28ea46
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-02-01 19:34:08 -0800

    add user profile to index

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Router, Route } from 'react-router'
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import LoginPage from './components/LoginPage'
-import MatchDetail from './components/MatchDetail'
 import store, { history } from './store/configure-store'
 import { fetchMatches } from './actions/matches-actions'
 import { checkIfLoggedIn } from './actions/user-actions'
+import App from './components/App';
+import LoginPage from './components/LoginPage'
+import MatchDetail from './components/MatchDetail'
+import UserProfile from './components/UserProfile'
 import './styles/index.css';
 
 store.dispatch(fetchMatches())
@@ -20,6 +21,7 @@ const Root = () => {
         <Route path="/" component={App} />
         <Route path="/login" component={LoginPage} />
         <Route path="/matches/:matchId" component={MatchDetail} />
+        <Route path="/profile" component={UserProfile} />
       </Router>
     </Provider>
   )
