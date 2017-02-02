ee76db8839bda2131e53f78da3d382f38778878d
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-02-01 20:35:16 -0800

    trying to style the list

--- src/components/Match.js
+++ src/components/Match.js
@@ -3,23 +3,54 @@ import { Link } from 'react-router'
 import '../styles/Match.css';
 
 class Match extends Component {
-  render() {
-    const { id, homeTeam, awayTeam, date, time, matchType, location, ticketPrice } = this.props.matchData
-    return (
+  constructor(props) {
+    super(props)
 
-        <div>
-          <Link to={`/matches/${id}`} onClick={() => this.props.onMatchClick(id)} >
-          <div className="desc" >
-            <h3>{awayTeam.name}</h3>
-            <p>
-              {date.toUpperCase()} - {time}
-            </p>
-             <p>
-              ${(ticketPrice / 100).toFixed(2)}
-            </p>
-          </div>
-        </Link>
+    this.renderMatchCard = this.renderMatchCard.bind(this)
+    this.renderMatchCondensed = this.renderMatchCondensed.bind(this)
+  }
+
+  renderMatchCard() {
+    const { id, homeTeam, awayTeam, date, time, matchType, location, ticketPrice } = this.props.matchData
+
+    return (
+      <div>
+        <Link to={`/matches/${id}`} onClick={() => this.props.onMatchClick(id)} >
+        <div className="desc" >
+          <h3>{awayTeam.name}</h3>
+          <p>
+            {date.toUpperCase()} - {time}
+          </p>
+           <p>
+            ${(ticketPrice / 100).toFixed(2)}
+          </p>
         </div>
+      </Link>
+      </div>
+    )
+  }
+
+  renderMatchCondensed() {
+    const { id, homeTeam, awayTeam, date, time, matchType, location, ticketPrice } = this.props.matchData
+
+    return (
+      <div className="match-condensed ">
+        <h3 className="match-condensed-heading">{awayTeam.name} vs.</h3>
+        <h3 className="match-condensed-subheading soft-grey-text">{homeTeam.name}</h3>
+        <h3 className="match-condensed-subheading soft-grey-text">CENTER BLOCK VERTICALLY IN BOX</h3>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.props.condensed
+          ? this.renderMatchCondensed()
+          : this.renderMatchCard()
+        }
+      </div>
     )
   }
 }
---
+++ src/components/UserProfile.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import NavBar from './NavBar'
+import Match from './Match'
+
+class UserProfile extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="match-detail-container">
+          <div className="match-detail-item">
+            <h2>Your Matches</h2>
+            <ul>
+              {
+                this.props.userMatches.map(match =>
+                  <li key={match.id}>
+                    <Match key={match.id} matchData={match} condensed/>
+                  </li> )
+              }
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const filterUserMatches = (match, userid) => match.claimedUserId === userid
+const reduceMatches = (matches) => (a, b) => a.concat([matches[b]])
+
+const mapStateToProps = (state) => {
+  //TODO -- this should go into a selector
+  const { matches } = state
+  const { user } = state.user
+  const matchReducer = reduceMatches(matches.data)
+  const filteredMatches = Object.keys(matches.data).reduce(matchReducer, []).filter((match) => filterUserMatches(match, user.uid))
+
+  return {
+    userMatches: filteredMatches
+  }
+}
+
+const UserProfileContainer = connect(mapStateToProps)(UserProfile)
+
+export default UserProfileContainer;
