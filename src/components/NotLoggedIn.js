526780ec1a7d55aa96ae6a59532cc85ff88a9621
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-02-04 11:17:35 -0800

    updated verbage

--- src/components/NotLoggedIn.js
+++ src/components/NotLoggedIn.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router'
 class NotLoggedIn extends Component {
   render() {
     return (
-      <div>
-        <h1> Oh no! You're not logged in.</h1>
-        <Link to="/login"> Login </Link>
+      <div >
+        <h1 className="match-detail-title"> Oh no! You're not logged in.</h1>
+        <h3 className="medium-grey-text centered">You need to be logged in to see this page</h3>
       </div>
     )
   }
