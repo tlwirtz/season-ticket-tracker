e6ece5e7625856aee1697fe5665003aa0f7fe391
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-02-07 17:40:40 -0800

    footer template

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>This is my footer</h4>
+      </div>
+    )
+  }
+}
+
+export default Footer;
