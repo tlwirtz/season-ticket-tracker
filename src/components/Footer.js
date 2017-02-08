5cf526af08b139f75928d436b684fad6cc679dcb
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-02-07 18:43:57 -0800

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import '../styles/Footer.css'
 
 class Footer extends Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     return (
-      <div>
-        <h4>This is my footer</h4>
+      <div className="footer-container">
+        <div className="footer-item">
+          <h4 className="soft-grey-text">&copy; 2017 - <a href="http://greysky.io">greysky.io</a></h4>
+        </div>
+        <div className='footer-item'>
+          <a href="https://twitter.com/wirtzt">
+            <h4 className='center-text'>
+              <i className="fa fa-twitter fa-2x" aria-hidden="true"></i>
+            </h4>
+          </a>
+        </div>
+        <div className='footer-item'>
+          <a href="https://github.com/tlwirtz">
+            <h4 className='center-text'>
+              <i className="fa fa-github fa-2x" aria-hidden="true"></i>
+            </h4>
+          </a>
+        </div>
+
       </div>
     )
   }
