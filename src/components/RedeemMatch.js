a416f804cc5b6ac8f4f7beb359d7e22a181cf5f9
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-02-11 13:47:24 -0800

    redeem match component

---
+++ src/components/RedeemMatch.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { updateMatchReq } from '../actions/matches-actions'
+
+class RedeemMatch extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      redemptionCode: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClaimTicket = this.handleClaimTicket.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleClaimTicket(e) {
+    e.preventDefault();
+    const { user, matchId } = this.props
+    const { redemptionCode } = this.state
+    if (user.uid) {
+      return this.props.claimTicket(matchId, user.uid, redemptionCode)
+    }
+
+    return false
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          className="action-button claim-ticket-button"
+          onClick={ (e) => this.handleClaimTicket(e) }
+          >
+            Claim Ticket
+        </button>
+        <input
+          type="text"
+          name="redemptionCode"
+          value={this.state.redemptionCode}
+          onChange={(e) => this.handleChange(e) }/>
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    claimTicket: (matchId, userId, redemptionCode) => {
+      const payload = { claimedUserId: userId, available: false }
+      dispatch(updateMatchReq(matchId, payload, redemptionCode))
+    }
+  }
+}
+
+const RedeemMatchContainer = connect(null, mapDispatchToProps)(RedeemMatch)
+
+export default RedeemMatchContainer;
