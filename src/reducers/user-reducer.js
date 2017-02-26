e22938eae2e51648442ec4339fd2926127d6d8ad
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-02-26 11:41:53 -0800

    explicitly set user to null

--- src/reducers/user-reducer.js
+++ src/reducers/user-reducer.js
@@ -5,9 +5,10 @@ export default (state={}, action) => {
     case types.USER_LOGIN_REQUEST_SUCCESS:
       return { ...state, user: action.payload.user, credential: action.payload.credential }
     case types.USER_LOGOUT_REQUEST_SUCCESS:
-      return { }
     case types.USER_LOGIN_REQUEST:
+      return state
     case types.USER_LOGOUT_REQUEST:
+      return { ...state, user: null, credential: null }
     default:
       return state;
   }
