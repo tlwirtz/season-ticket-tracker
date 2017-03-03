dd095fed9b34642e29c83171ef81d96c17f5d9f9
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-03-02 19:40:10 -0800

    active voice for test descriptions

--- src/actions/tests/user-actions.test.js
+++ src/actions/tests/user-actions.test.js
@@ -1,37 +1,53 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-import nock from 'nock'
 import * as actions from '../user-actions'
 
 const middlewares = [ thunk ]
 const mockStore = configureMockStore(middlewares)
 
-//TODO -- how to handle mocking of REBASE functionality?
+jest.mock('../../base')
+import base from '../../base'
+
+base.authWithOAuthPopup = jest.fn((provider, authHandler) => {
+  if (provider === 'bad-provider') return authHandler({ err: 'bad provider' })
+  return authHandler(null, {user: { id: 'taylor' } })
+})
+
+jest.mock('../../store/configure-store')
+import * as config from '../../store/configure-store'
+config.history = { push: jest.fn() }
+
+const mapActions = (act) => act.type
 
 describe('user actions', () => {
+  beforeEach(() => {
+    localStorage.setItem.mockClear();
+    config.history.push.mockClear();
+  })
+
   describe('userLogin', () => {
-    it('should return an action', () => {
+    it('returns an action', () => {
       const expected = { type: actions.USER_LOGIN_REQUEST}
       expect(actions.userLogin()).toEqual(expected)
     })
   })
 
   describe('userLogout', () => {
-    it('should return an action', () => {
+    it('returns an action', () => {
       const expected = { type: actions.USER_LOGOUT_REQUEST}
       expect(actions.userLogout()).toEqual(expected)
     })
   })
 
   describe('userLogoutSuccess', () => {
-    it('should return an action', () => {
+    it('returns an action', () => {
       const expected = { type: actions.USER_LOGOUT_REQUEST_SUCCESS}
       expect(actions.userLogoutSuccess()).toEqual(expected)
     })
   })
 
   describe('userLoginSuccess', () => {
-    it('should return an action with auth data', () => {
+    it('returns an action with auth data', () => {
       const payload = {
         user: 'test user',
         credential: 'test creds'
@@ -46,7 +62,7 @@ describe('user actions', () => {
   })
 
   describe('userLoginFailuer', () => {
-    it('should return an action with error', () => {
+    it('returns an action with error', () => {
       const expected = {
         type:  actions.USER_LOGIN_REQUEST_FAILURE,
         payload: {
@@ -60,7 +76,7 @@ describe('user actions', () => {
   })
 
   describe('userLogoutReq', () => {
-    it('should logout the user', () => {
+    it('logs out the user', () => {
       const store = mockStore({})
       const expected = [
         { type: actions.USER_LOGOUT_REQUEST },
@@ -69,32 +85,78 @@ describe('user actions', () => {
       store.dispatch(actions.userLogoutReq())
       expect(store.getActions()).toEqual(expected)
     })
+
+    xit('should clear local storage', () => {})
+    xit('should make a call to base.unauth', () => {})
   })
 
-  //TODO -- try actually mocking by using nock ...
-  //TODO -- figure out which calls
   describe('userLoginReq', () => {
-    it('should log in a user', () => {
+    it('dispatches the success actions if okay', () => {
       const store = mockStore({})
-      return store.dispatch(actions.userLoginReq('google'))
-        .then(data => expect(data).toEqual(null))
+      const expected = [
+        'USER_LOGIN_REQUEST',
+        'USER_LOGIN_REQUEST_SUCCESS',
+      ]
+
+      store.dispatch(actions.userLoginReq('google'))
+      const dispatched = store.getActions()
+      expect(dispatched.map(mapActions)).toEqual(expected)
+    })
+
+    it('dispatches the error actions if error', () => {
+      const store = mockStore({})
+      const expected = [
+        'USER_LOGIN_REQUEST',
+        'USER_LOGIN_REQUEST_FAILURE',
+      ]
+
+      store.dispatch(actions.userLoginReq('bad-provider'))
+      const dispatched = store.getActions()
+      expect(dispatched.map(mapActions)).toEqual(expected)
+    })
+
+    it('correctly sets localStorage if user logged in', () => {
+      const store = mockStore({})
+      const user = { user: { id: 'taylor' } }
+      const expected = [
+        ['user', JSON.stringify(user)]
+      ]
+
+      store.dispatch(actions.userLoginReq('google'))
+      expect(localStorage.setItem.mock.calls).toEqual(expected)
+    })
+
+    it('does not call localStorage or history', () => {
+      const store = mockStore({})
+
+      store.dispatch(actions.userLoginReq('bad-provider'))
+      expect(localStorage.setItem.mock.calls.length).toEqual(0)
+      expect(config.history.push.mock.calls.length).toEqual(0)
+    })
+
+    it('correctly sets the history if user logged in', () => {
+      const store = mockStore({})
+      const expected = [ ['/'] ]
+
+      store.dispatch(actions.userLoginReq('google'))
+      expect(config.history.push.mock.calls).toEqual(expected)
     })
   })
 
   describe('userLoginLocalStorage', () => {
-    xit('should login if credentials are in LS', () => {
+    xit('logs in if credentials are in LS', () => {
 
     })
   })
 
   describe('checkIfAdmin', () => {
-    xit('should check if a user is an admin', () => {
+    xit('checks if a user is an admin', () => {
 
     })
   })
 
   describe('checkifLoggedIn', () => {
-    xit('should check if the user is in local storage', () => {
+    xit('checks if the user is in local storage', () => {
 
     })
   })
