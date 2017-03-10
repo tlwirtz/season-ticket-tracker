907acd3bcbd4e0abbe7874d93761e62154d442ce
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-03-09 20:33:06 -0800

    tests for reducers

---
+++ src/reducers/tests/user-reducer.test.js
@@ -0,0 +1,41 @@
+import * as types from '../../actions/user-actions'
+import reducer from '../user-reducer'
+
+describe('reducer', () => {
+  it('should handle USER_LOGIN_REQUEST_SUCCESS', () => {
+    const state = {}
+    const payload = {
+      user: 'test-user',
+      credential: 'test-cred'
+    }
+
+    const action = {
+      type: types.USER_LOGIN_REQUEST_SUCCESS,
+      payload
+    }
+
+    expect(reducer(state, action)).toEqual(payload)
+  })
+
+  it('should handle USER_LOGOUT_REQUEST_SUCCESS', () => {
+    const state = {}
+    const action = { type: types.USER_LOGOUT_REQUEST_SUCCESS }
+
+    expect(reducer(state, action)).toEqual(state)
+  })
+
+  it('should handle USER_LOGIN_REQUEST', () => {
+    const state = {}
+    const action = { type: types.USER_LOGIN_REQUEST }
+
+    expect(reducer(state, action)).toEqual(state)
+  })
+
+  it('should handle USER_LOGOUT_REQUEST', () => {
+    const state = { user: 'test', credential: 'hell-cred' }
+    const action = { type: types.USER_LOGOUT_REQUEST }
+    const expectedState = { user: null, credential: null }
+
+    expect(reducer(state, action)).toEqual(expectedState)
+  })
+})
