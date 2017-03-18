fd920aeccc993631c1f97d932f2b029e47feb7ff
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-03-18 11:25:02 -0700

    ofilter out matches that are unavailable

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -6,27 +6,55 @@ import { selectMatch } from '../actions/matches-actions';
 import Match from './Match';
 
 export class MatchList extends Component {
+  constructor(props) {
+    super(props)
+    this.buildMatch = this.buildMatch.bind(this)
+    this.filterMatch = this.filterMatch.bind(this)
+  }
+
+  filterMatch(isAvailable) {
+    return (key) => this.props.matches[key].available === isAvailable
+  }
+
+  buildMatch(key) {
+    const bg = this.props.matches[key].awayTeam.img;
+    return (
+      <Link
+        key={key} to={`/matches/${key}`}
+        className='match animated fadeInUp'
+        style={{ backgroundImage: `url(${bg})` }}
+        onClick={() => this.props.onMatchClick(key)} >
+        <Match
+          matchData={this.props.matches[key]}
+          {...this.props} />
+      </Link>
+    );
+  }
 
   render() {
-    const items = Object.keys(this.props.matches).map((key) => {
-      const bg = this.props.matches[key].awayTeam.img;
-      return (
-        <Link
-          key={key} to={`/matches/${key}`}
-          className='match animated fadeInUp'
-          style={{backgroundImage: `url(${bg})`}}
-          onClick={() => this.props.onMatchClick(key)} >
-            <Match
-              matchData={this.props.matches[key]}
-              {...this.props}/>
-        </Link>
-      );
-    }
-  );
+    const availableGames = Object.keys(this.props.matches)
+      .filter(this.filterMatch(true))
+      .map(this.buildMatch);
+
+    const reservedGames = Object.keys(this.props.matches)
+      .filter(this.filterMatch(false))
+      .map(this.buildMatch);
+
     return (
-      <div className='match-container'>
-        { items }
-      </div>
+      <section>
+        <div className='match-container'>
+          <h3 className="extra-left-margin nav-bar-subheading soft-grey-text" >Available matches</h3>
+        </div>
+        <div className='match-container'>
+          {availableGames}
+        </div>
+        <div className='match-container'>
+          <h3 className="extra-left-margin nav-bar-subheading soft-grey-text" >Unavailable Matches</h3>
+        </div>
+        <div className='match-container'>
+          {reservedGames}
+        </div>
+      </section>
     );
   }
 }
