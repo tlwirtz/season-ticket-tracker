6fb8798f7b24c409fca59750dffbd75005d42593
Taylor Wirtz <taylor.wirtz@gmail.com>
2024-07-11 18:19:32 -0700

    migrations init

---
+++ migrations/0002_complete_santa_claus.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "matches" ALTER COLUMN "available" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "matches" ALTER COLUMN "away_team" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "matches" ALTER COLUMN "home_team" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "matches" ALTER COLUMN "matchType" SET NOT NULL;
