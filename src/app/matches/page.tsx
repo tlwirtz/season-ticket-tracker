2cdf5c83acf688c940fbd0504543043435108b4b
Taylor Wirtz <taylor.wirtz@gmail.com>
2024-07-14 16:15:21 -0700

    stub out what it's like to use dirzzle

--- src/app/matches/page.tsx
+++ src/app/matches/page.tsx
@@ -1,3 +1,29 @@
-export default function Matches() {
-    return <p>Matches!</p>;
+import { db } from '../../../db/db';
+import { matchTable, teamTable } from '../../../db/schema';
+import { eq, aliasedTable } from 'drizzle-orm';
+
+const awayTeam = aliasedTable(teamTable, 'awayTeam');
+
+export default async function Matches() {
+    const matches = await db
+        .select()
+        .from(matchTable)
+        .leftJoin(teamTable, eq(matchTable.homeTeam, teamTable.id))
+        .leftJoin(awayTeam, eq(matchTable.awayTeam, awayTeam.id));
+
+    return (
+        <div>
+            {/* <p>{JSON.stringify(matches[0])}</p> */}
+            {matches.length &&
+                matches.map(match => {
+                    return (
+                        <p>
+                            MatchId: {match.matches.id} - {match.teams?.name ?? ''} vs.{' '}
+                            {match.awayTeam?.name}
+                            {/* Not sure how to get this ^ type error to go away. */}
+                        </p>
+                    );
+                })}
+        </div>
+    );
 }
