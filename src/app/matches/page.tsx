fa73b6f7712654a755b2a73ce5859ab0274fcf8e
Taylor Wirtz <taylor.wirtz@gmail.com>
2024-07-22 19:56:22 -0700

    beginning migration of the UI

--- src/app/matches/page.tsx
+++ src/app/matches/page.tsx
@@ -1,29 +1,30 @@
 import { db } from '../../../db/db';
 import { matchTable, teamTable } from '../../../db/schema';
 import { eq, aliasedTable } from 'drizzle-orm';
+import MatchList from '../../components/MatchList';
+import '../../../styles/Match.css';
 
 const awayTeam = aliasedTable(teamTable, 'awayTeam');
+const homeTeam = aliasedTable(teamTable, 'homeTeam');
 
 export default async function Matches() {
+    /**
+     * Returns an array of objects like:
+     * [{
+     *  matches: {...matchData},
+     *  homeTeam: {...team}
+     *  awayTeam: {...team}
+     * }]
+     */
     const matches = await db
         .select()
         .from(matchTable)
-        .leftJoin(teamTable, eq(matchTable.homeTeam, teamTable.id))
+        .leftJoin(homeTeam, eq(matchTable.homeTeam, homeTeam.id))
         .leftJoin(awayTeam, eq(matchTable.awayTeam, awayTeam.id));
 
     return (
-        <div>
-            {/* <p>{JSON.stringify(matches[0])}</p> */}
-            {matches.length &&
-                matches.map(match => {
-                    return (
-                        <p>
-                            MatchId: {match.matches.id} - {match.teams?.name ?? ''} vs.{' '}
-                            {match.awayTeam?.name}
-                            {/* Not sure how to get this ^ type error to go away. */}
-                        </p>
-                    );
-                })}
-        </div>
+        <>
+            <MatchList matchData={matches} />
+        </>
     );
 }
