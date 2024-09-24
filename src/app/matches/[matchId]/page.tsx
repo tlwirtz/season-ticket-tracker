1858abb29fbbdabed72a60cb5b6839d0ea4325df
Taylor Wirtz <taylor.wirtz@gmail.com>
2024-09-23 22:28:11 -0700

    redirect to /matches if bad match-id

--- src/app/matches/[matchId]/page.tsx
+++ src/app/matches/[matchId]/page.tsx
@@ -1,6 +1,7 @@
 import { db } from '../../../../db/db';
 import { matchTable, teamTable, redemptionCodeTable } from '../../../../db/schema';
 import { eq, aliasedTable } from 'drizzle-orm';
+import { redirect } from 'next/navigation';
 import MatchDetail from '../../../components/MatchDetail';
 
 const awayTeam = aliasedTable(teamTable, 'awayTeam');
@@ -12,7 +13,9 @@ export default async function Page({ params }: { params: { matchId: number } }) 
      * Afterwards, we can load it on the page
      */
 
-    const user = { userId: 939394839, uid: 9393920293, name: 'taylor' }; //need to figure out how to populate this
+    //todo -- this should get the user from the auth context
+    //todo -- I'm not even sure if we need to inject this in.
+    const user = { userId: 939394839, uid: 9393920293, name: 'taylor' };
     const [matchData] = await db
         .select()
         .from(matchTable)
@@ -20,6 +23,10 @@ export default async function Page({ params }: { params: { matchId: number } }) 
         .leftJoin(awayTeam, eq(matchTable.awayTeam, awayTeam.id))
         .where(eq(matchTable.id, params.matchId));
 
+    if (!matchData) {
+        //probably a bad match id
+        redirect('/matches');
+    }
     const matchMapped = {
         ...matchData.matches,
         homeTeam: matchData.homeTeam, //todo -- figure out how to fix type errors
