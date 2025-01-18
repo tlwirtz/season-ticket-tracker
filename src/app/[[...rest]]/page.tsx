d2cd089f6444ee7026d825914d530b83b547bf32
Taylor Wirtz <taylor.wirtz@gmail.com>
2025-01-18 09:05:59 -0800

    show matches even when logged out

--- src/app/[[...rest]]/page.tsx
+++ src/app/[[...rest]]/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     return (
         <>
             <SignedOut>
-                <Landing />
+                <Matches />
             </SignedOut>
             <SignedIn>
                 <Matches />
