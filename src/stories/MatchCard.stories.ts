e4880b2cd5d2760dfdd52df0cfb2cc2ae038dd47
Taylor Wirtz <taylor.wirtz@gmail.com>
2025-02-26 16:14:16 -0800

    fix arg types

--- src/stories/MatchCard.stories.ts
+++ src/stories/MatchCard.stories.ts
@@ -33,10 +33,7 @@ const meta = {
     // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
     tags: ['autodocs'],
     // More on argTypes: https://storybook.js.org/docs/api/argtypes
-    argTypes: {
-        match: testMatch,
-        useAdminLayout: true
-    },
+    argTypes: {},
     // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
     args: { match: testMatch, useAdminLayout: true }
 } satisfies Meta<typeof MatchCard>;
