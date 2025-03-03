47e5d28a15b9fd561f29cbf4f7cf8f73bc8c0445
Taylor Wirtz <taylor.wirtz@gmail.com>
2025-03-03 14:06:04 -0800

    cleanup - add typescript types

--- types/globals.d.ts
+++ types/globals.d.ts
@@ -10,6 +10,19 @@ declare global {
         };
     }
 
+    namespace NodeJS {
+        interface ProcessEnv {
+            DATABASE_URL: string;
+            NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
+            CLERK_SECRET_KEY: string;
+            TZ: string;
+            NEXT_PUBLIC_USE_STRIPE: string;
+            NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
+            NEXT_PUBLIC_STRIPE_PRICING_TABLE_ID: string;
+            STRIPE_ENDPOINT_SECRET: string;
+            STRIPE_SECRET_KEY: string;
+        }
+    }
     namespace JSX {
         interface IntrinsicElements {
             'stripe-pricing-table': React.DetailedHTMLProps<
