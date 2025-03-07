c835bc8c1e990f83bfe77cb77e68899eaed5331e
Taylor Wirtz <taylor.wirtz@gmail.com>
2025-03-07 18:38:03 +0000

    separate testing setup for browser testing vs. integration testing

--- test/browser-setup.ts
+++ test/browser-setup.ts
@@ -1,6 +1,8 @@
+import '@testing-library/jest-dom/vitest';
 import { PGlite } from '@electric-sql/pglite';
 import { drizzle } from 'drizzle-orm/pglite';
 import { afterAll, afterEach, beforeEach, vi } from 'vitest';
+import { cleanup } from '@testing-library/react';
 
 import * as schema from '../db/schema';
 import { db } from '../db/db';
@@ -8,11 +10,11 @@ import { main } from '../db/seed';
 import { applyMigrations } from '../db/migrate';
 import { sql } from 'drizzle-orm';
 
-const client = new PGlite('./test/.integration-db/test.db');
+const client = new PGlite('./test/.browser-db/test.db');
 
 // Replace the database with a new in-memory database
 vi.mock('../db/db.ts', async => {
-    const pool = new PGlite('./test/.integration-db/test.db');
+    const pool = new PGlite('./test/.browser-db/test.db');
     const db = drizzle(pool, { schema });
     return {
         db,
@@ -31,6 +33,7 @@ afterEach(async () => {
     await db.execute(sql`drop schema if exists public cascade`);
     await db.execute(sql`create schema public`);
     await db.execute(sql`drop schema if exists drizzle cascade`);
+    cleanup();
 }, 15000);
 
 // Free up resources after all tests are done
